41f3f62a4b38e365f50a4908f62ba45667362a17
SHREYASTELKAR <you@exa>
2024-02-13 20:44:07 -0800

    sizes

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,17 +1,49 @@
-import { View, Text } from 'react-native';
-import { styles } from '@/src/components/ProductsList';
+import { View, Text, Image, StyleSheet} from 'react-native';
+import { defaultPizzaImage } from '@/src/components/ProductsList';
 import { useLocalSearchParams } from 'expo-router';
 import {Stack} from 'expo-router';
+import products from '@/assets/Food Ordering Asset bundle/data/products';
+
+const sizes = ['S', 'M', 'L', 'XL'];
+
 const product = () => {
   const { id } = useLocalSearchParams();
+  const product = products.find((p) => p.id.toString() === id);
+
+  if (!product) {
+    return <Text> Product not found </Text>;
+  }
 
   return (
     <View>
 
-      <Stack.Screen options={{ title: 'Details' + id }} />
-      <Text style={styles.my_title}> product details for id: {id}</Text>
+      <Stack.Screen options={{ title: product?.name }} />
+      <Image source={{ uri: product.image || defaultPizzaImage}} style={styles.image}/>
+
+      <Text> Select Size </Text>
+      {sizes.map((size) => (
+        <Text key={size} > {size} </Text>
+      ))}
+
+      <Text style={styles.price}> ${product.price}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create ({
+  container: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+
+});
 
 export default product;
